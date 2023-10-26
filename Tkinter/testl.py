0acde6d49c93dcd7fede76259e55f1336f3d3134
Soumendu <118378045+Soumendu22@users.noreply.github.com>
2023-10-26 23:18:11 +0530

    Add files via upload

--- Tkinter/testl.py
+++ Tkinter/testl.py
@@ -4,13 +4,13 @@ import sqlite3
 import hashlib
 import subprocess
 
-class SignupGUI(tk.Tk):
+class LoginGUI(tk.Tk):
     global self
     def __init__(self):
         
         super().__init__()
 
-        self.title("User signup")
+        self.title("User login")
         width=600
         height=400
         screenwidth = self.winfo_screenwidth()
@@ -20,10 +20,9 @@ class SignupGUI(tk.Tk):
         self.resizable(width=False, height=False)
         self.config(bg='white')
 
-
         # Title Label
         title_lbl = tk.Label(self, text="Attendance Manager", font=("Arial", 18, "bold"), bg='white')
-        title_lbl.pack(pady=1)
+        title_lbl.pack(pady=10)
 
         # Username
         username_lbl = tk.Label(self, text="Username", font=("Arial", 15), bg='white')
@@ -31,7 +30,7 @@ class SignupGUI(tk.Tk):
 
         global username_entry
         username_entry = tk.Entry(self, font=("Arial", 16), width=30, bd=2, relief='flat', fg='grey')
-        username_entry.pack(pady=(0,10), ipady=2, padx=5)
+        username_entry.pack(pady=(0,20), ipady=2, padx=5)
         username_entry.config(highlightthickness=2, highlightbackground="grey")
         username_entry.insert(0, "Your username")
         username_entry.placeholder = "Your username"
@@ -44,77 +43,58 @@ class SignupGUI(tk.Tk):
 
         global password_entry
         password_entry = tk.Entry(self, font=("Arial", 16), width=30, show="", bd=2, relief='flat', fg='grey')
-        password_entry.pack(pady=(0,10), ipady=1, padx=5)
+        password_entry.pack(pady=(0,20), ipady=1, padx=5)
         password_entry.config(highlightthickness=2, highlightbackground="grey")
         password_entry.insert(0, "Your Password")
         password_entry.placeholder = "Your Password"
         password_entry.bind("<FocusIn>", clear_placeholder)
         password_entry.bind("<FocusOut>", replace_placeholder)
 
-        # Email
-        email_lbl = tk.Label(self, text="Email", font=("Arial", 15), bg='white')
-        email_lbl.pack(pady=(0,5))
+        
 
-        global email_entry 
-        email_entry = tk.Entry(self, font=("Arial", 16), width=30, bd=2, relief='flat', fg='grey')
-        email_entry.pack(pady=(0,10), ipady=1, padx=5)
-        email_entry.config(highlightthickness=2, highlightbackground="grey")
-        email_entry.insert(0, "Your email")
-        email_entry.placeholder = "Your email"
-        email_entry.bind("<FocusIn>", clear_placeholder)
-        email_entry.bind("<FocusOut>", replace_placeholder)
-
-        # Sign up button
-        signup_button = tk.Button(self, text="Sign up", font=("Arial", 16, "bold"), bg="black", fg="white", width=15, height=1, bd=0, relief='flat', command=signup)
-        signup_button.pack(pady=8)
+        # Login in button
+        login_button = tk.Button(self, text="Log in", font=("Arial", 16, "bold"), bg="black", fg="white", width=15, height=1, bd=0, relief='flat', command=login)
+        login_button.pack(pady=8)
 
         # Login Button
-        login_btn = tk.Button(self, text="Already have an account? Log in", font=("Arial", 14), bg='white', relief='flat', bd=0, activebackground='white', foreground='blue', activeforeground='blue', cursor="hand2")
-        login_btn.pack(pady=10)
+        signup_btn = tk.Button(self, text="Don't have an account? Sign up", font=("Arial", 14), bg='white', relief='flat', bd=0, activebackground='white', foreground='blue', activeforeground='blue', cursor="hand2")
+        signup_btn.pack(pady=10)
         def open_login_page(event):
             self.destroy()
-            file_path = 'testl.py'
+            file_path = 'test.py'
             try:
                 subprocess.Popen(['python', file_path])
             except FileNotFoundError:
                 print("File not found.")
-        login_btn.bind("<Button-1>", open_login_page) 
+        signup_btn.bind("<Button-1>", open_login_page) 
 
 
 def is_valid_email(email):
     # Check if the email contains "@" symbol
     return "@" in email
 
-def signup():
+def login():
     username = username_entry.get()
     password = hashlib.sha256(password_entry.get().encode()).hexdigest()
-    email = email_entry.get()
 
     # Check if any of the fields are empty
-    if not username or not password or not email:
+    if not username or not password:
         messagebox.showinfo("All fields are required","Please fill all details")
-        return
-    if not is_valid_email(email):
-        messagebox.showinfo("please include(@)","Invalid email format (missing @ symbol)")
         return
 
     conn = sqlite3.connect('user_database.db')
     cursor = conn.cursor()
 
-     # Check if the username is already in use
-    cursor.execute('SELECT username FROM users WHERE username = ?', (username,))
-    existing_user = cursor.fetchone()
+    cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+    user = cursor.fetchone()
 
-    if existing_user:
-        messagebox.showinfo("Change username","Username already exists")
-    else:
-        cursor.execute('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', (username, password, email))
-        conn.commit()
-        conn.close()
-        messagebox.showinfo("Welcome!!!","Signup successful")
-        
+    if user:
+        messagebox.showinfo("Welcome!!!","Login successfull")
         open_manager()
         self.destroy()
+    else:
+        messagebox.showinfo("Wrong Information","Invalid username or password")
+
 
 def open_manager():
     file_path = 'home.py'
@@ -143,6 +123,6 @@ def replace_placeholder(event):
 
 
 
-app = SignupGUI()
+app = LoginGUI()
 app.mainloop()
 
